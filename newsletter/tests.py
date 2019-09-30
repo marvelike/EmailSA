f95d08708231ca81e186e13bbdd64e2e5338d55a
marvelike <marvelike7@gmail.com>
2019-09-30 21:05:53 +0100

    writing tests

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import Content, Footer, Header
 # Create your tests here.
+
+class HeaderTest(TestCase):
+    """Test Header class module"""
+
+    def setUp(self):
+        Header.objects.create(email_from='dummymail@gmail.com', email_title='Hello there')
