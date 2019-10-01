d9d585f9f2d97e73449dc608a29550e79d352b27
marvelike <marvelike7@gmail.com>
2019-10-01 15:48:28 +0100

    Branch switched to master

--- EmailSa/settings.py
+++ EmailSa/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newsletter',
+    'rest_framework'
+
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
